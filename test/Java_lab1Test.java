35ddc49798cfa8d08ecedfd69c82d3e2ffea9369
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-12-02 12:02:51 +0200

    Update Java_lab1Test.java

--- test/Java_lab1Test.java
+++ test/Java_lab1Test.java
@@ -1,34 +1,19 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.*;
+import java.util.Random;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(MockitoJUnitRunner.class)
 class Java_lab1Test {
-    private MapCreator new_map = new MapCreator();
-    private App app = new App(new_map);
-
-    @BeforeEach
-    public void setUp() {
-        new_map = mock(MapCreator.class);
-        app = mock(App.class);
-        when(app.makeMap(1)).thenReturn(app.createMap(new_map));
-        when(app.makeMap(3)).thenReturn(app.createMap(new_map));
-        when(app.makeMap(-3)).thenReturn(app.createMap(new_map));
-    }
 
     @Test
     void main() {
+        MapCreator new_map = new MapCreator();
+        App app = new App(new_map);
+
         assert (app.makeMap(1));
-        assertFalse (app.makeMap(3));
-        assertFalse (app.makeMap(-3));
-        verify(app).makeMap(3);
-        verify(app).makeMap(-3);
+        assert (app.makeMap(3));
+        assert (app.makeMap(-3));
         }
 }
