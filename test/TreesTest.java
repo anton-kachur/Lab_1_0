2c3e4405cfe5f5457231da00fe0626843a0a57b2
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-12-02 12:06:57 +0200

    Update TreesTest.java

--- test/TreesTest.java
+++ test/TreesTest.java
@@ -1,27 +1,12 @@
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(MockitoJUnitRunner.class)
 class TreesTest {
-    private Trees test = new Trees("Thuringen Wald", "Forest");
-
-    @BeforeEach
-    public void setUp() {
-        test = mock(Trees.class);
-        when(test.printAll()).thenReturn("Name: Thuringen Wald\nType: Forest");
-
-    }
 
     @Test
     void printAll() {
+        Trees test = new Trees("Thuringen Wald", "Forest");
         assertEquals("Name: Thuringen Wald\nType: Forest", test.printAll());
-        verify(test, atLeastOnce()).printAll();
     }
 }
