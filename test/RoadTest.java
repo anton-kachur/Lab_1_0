2d985c9630b2696fe281efd3131eaf4bb67b4d07
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-12-02 12:05:48 +0200

    Update RoadTest.java

--- test/RoadTest.java
+++ test/RoadTest.java
@@ -1,28 +1,12 @@
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(MockitoJUnitRunner.class)
 class RoadTest {
-    private Road test = new Road("Dirt", 1);
-
-    @BeforeEach
-    public void setUp() {
-        test = mock(Road.class);
-        when(test.printAll()).thenReturn("Road type: Dirt\nNumber of lanes: 1");
-
-    }
 
     @Test
-    void printAll() {
-
+    void test() {
+        Road test = new Road("Dirt", 1);
         assertEquals("Road type: Dirt\nNumber of lanes: 1", test.printAll());
-        verify(test).printAll();
     }
 }
