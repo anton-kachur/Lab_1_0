90d51587f7d2fa86fd40968b535cf2171de3f3a1
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-12-02 12:05:20 +0200

    Update ReservoirTest.java

--- test/ReservoirTest.java
+++ test/ReservoirTest.java
@@ -1,27 +1,12 @@
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(MockitoJUnitRunner.class)
 class ReservoirTest {
 
-    private Reservoir test = new Reservoir("Swamp", 123.0);
-
-    @BeforeEach
-    public void setUp() {
-        test = mock(Reservoir.class);
-        when(test.printAll()).thenReturn("Reservoir type: Swamp\nDepth: 123.0");
-    }
-
     @Test
-    void printAll() {
+    void test() {
+        Reservoir test = new Reservoir("Swamp", 123.0);
         assertEquals("Reservoir type: Swamp\nDepth: 123.0", test.printAll());
-        verify(test).printAll();
     }
 }
